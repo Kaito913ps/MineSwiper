81f84960905bc9d7258fd6ed9b63d407850808c9
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-27 00:12:55 +0900

    cellを修正

--- Assets/cell.cs
+++ Assets/cell.cs
@@ -17,21 +17,36 @@ public enum CellState
 
     Mine = -1, // ’n—‹
 }
-public class cell : MonoBehaviour
+public class Cell : MonoBehaviour
 {
     [SerializeField]
     private Text _view = null;
 
     [SerializeField]
     private CellState _cellState = CellState.None;
-    void Start()
+
+    private void OnValidate()
     {
-        
+        OnCellStateChanged();
     }
 
-   
-    void Update()
+    private void OnCellStateChanged()
     {
-        
+        if (_view == null) { return; }
+
+        if (_cellState == CellState.None)
+        {
+            _view.text = "";
+        }
+        else if (_cellState == CellState.Mine)
+        {
+            _view.text = "X";
+            _view.color = Color.red;
+        }
+        else
+        {
+            _view.text = ((int)_cellState).ToString();
+            _view.color = Color.blue;
+        }
     }
 }
