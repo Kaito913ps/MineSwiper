6c8587a2ba390ff4f2009c95b7984fd45917e7ee
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-26 23:53:10 +0900

    cellのスクリプトを一旦セーブ

--- Assets/cell.cs
+++ Assets/cell.cs
@@ -1,16 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
+
+public enum CellState
+{
+    None = 0, // ‹óƒZƒ‹
+
+    One = 1,
+    Two = 2,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Six = 6,
+    Seven = 7,
+    Eight = 8,
+
+    Mine = -1, // ’n—‹
+}
 public class cell : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Text _view = null;
+
+    [SerializeField]
+    private CellState _cellState = CellState.None;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+   
     void Update()
     {
         
