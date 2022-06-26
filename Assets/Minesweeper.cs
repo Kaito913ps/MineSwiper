36cdbd797b807ac5e779728de428927aa55b3e7d
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-27 00:28:41 +0900

    Minesweeperのスクリプト

---
+++ Assets/Minesweeper.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Minesweeper : MonoBehaviour
+{
+    [SerializeField]
+    private GridLayoutGroup _gridLayoutGroup = null;
+
+    [SerializeField]
+    private Cell _cellPrefab = null;
+}
