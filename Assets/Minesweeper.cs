272397788960015ca8d172486b01ed1ea7cee26f
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-27 09:51:11 +0900

    とりあえずセーブ

--- Assets/Minesweeper.cs
+++ Assets/Minesweeper.cs
@@ -4,8 +4,27 @@ using UnityEngine.UI;
 public class Minesweeper : MonoBehaviour
 {
     [SerializeField]
+    private int _rows = 1;
+
+    [SerializeField]
+    private int _colums = 1;
+
+    [SerializeField]
     private GridLayoutGroup _gridLayoutGroup = null;
 
     [SerializeField]
     private Cell _cellPrefab = null;
+
+    private void Start()
+    {
+        var parent = _gridLayoutGroup.gameObject.transform;
+        for(var r = 0; r < _rows; r++)
+        {
+            for(var c = 0; c < _colums; c++)
+            {
+                var cell = Instantiate(_cellPrefab);
+                cell.transform.SetParent(parent);
+            }
+        }
+    }
 }
