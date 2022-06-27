b9003bcc538ddeb4ce3db71abb312938d3ae4be8
Kaito913ps <86725129+Kaito913ps@users.noreply.github.com>
2022-06-27 10:16:46 +0900

    これから課題をやる

--- Assets/Minesweeper.cs
+++ Assets/Minesweeper.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Minesweeper : MonoBehaviour
 {
     [SerializeField]
@@ -10,6 +11,9 @@ public class Minesweeper : MonoBehaviour
     private int _colums = 1;
 
     [SerializeField]
+    private int _mineCount = 1;
+
+    [SerializeField]
     private GridLayoutGroup _gridLayoutGroup = null;
 
     [SerializeField]
@@ -17,6 +21,10 @@ public class Minesweeper : MonoBehaviour
 
     private void Start()
     {
+        _gridLayoutGroup.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+        _gridLayoutGroup.constraintCount = _colums;
+
+        var _cells = new Cell[_rows, _colums];
         var parent = _gridLayoutGroup.gameObject.transform;
         for(var r = 0; r < _rows; r++)
         {
@@ -24,7 +32,16 @@ public class Minesweeper : MonoBehaviour
             {
                 var cell = Instantiate(_cellPrefab);
                 cell.transform.SetParent(parent);
+                _cells[r, c] = cell;
             }
+        }
+
+        for(var i = 0; i < _mineCount; i++)
+        {
+            var r = Random.Range(0, _rows);
+            var c = Random.Range(0, _colums);
+            var cell = _cells[r, c];
+            cell.CellState = CellState.Mine;
         }
     }
 }
